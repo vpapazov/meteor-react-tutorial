d17d40b874c71ff92994fd70a7083d349b50ba3c
valeria-hatchery <valeriia@hatchery.com>
2020-07-26 00:33:24 -0700

    first commit

---
+++ imports/ui/TaskForm.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Tasks from '/imports/api/tasks';
+
+export const TaskForm = ({ user }) => {
+    const [text, setText] = useState("");
+    
+    const handleSubmit = () => {
+        if (!text) return;
+
+        Tasks.insert({
+            text: text.trim(),
+            createdAt: new Date(),
+            owner: user._id,
+        });
+
+        setText("");
+    };
+
+
+    return (
+        <form className="task-form" onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Type to add new tasks"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
+
+            <button type="submit">Add Task</button>
+        </form>
+    );
+};
